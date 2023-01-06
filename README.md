e681c9eec39b70b68b8b9ff833f84262a1ca7c42
kamzone <macidlowskikacper20@lo3.gdynia.pl>
2023-01-06 20:15:48 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Simple script to send, list, and receive sms messages written in bash.
---
+++ sms.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ $1 == 'list' ]
+then
+	echo "Listing all messages id's and states."
+	mmcli -m 0 --messaging-list-sms
+elif [ $1 == 'read' ]
+then
+	echo "Showing message with selected id."
+	mmcli -m 0 -s /org/freedesktop/ModemManager1/SMS/${2}
+elif [ $1 == 'create' ]
+then
+	echo "Specify receipent number:"
+	read number
+	echo "Write your message:"
+	read message
+	mmcli -m 0 --messaging-create-sms="text='$message' ,number='$number'"
+elif [ $1 == 'send' ]
+then
+	echo "Sending message with selected id."
+	mmcli -s $2 --send
+elif [ $1 == 'delete' ]
+then
+	echo "Deleting message with selected id."
+	mmcli -m 0 --messaging-delete-sms=$2
+fi
