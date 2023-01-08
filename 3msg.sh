b25f5d3431b88a88e0c19b19ff890e64ff7f5116
kamzone <macidlowskikacper20@lo3.gdynia.pl>
2023-01-08 12:13:27 +0000

    Added variable to 3msg.sh that allows you to set modem_no easily

--- 3msg.sh
+++ 3msg.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 selector=$(echo -e "list\nread\ncreate\nsend\ndelete\n" | gum filter --placeholder "Select operation")
+modem_no=0 # Set modem number value, determined using mmcli -L (0 is usually correct)
 
 if [ $selector == 'list' ]
 then
 	echo "Listing all messages id's and states."
-	mmcli -m 0 --messaging-list-sms
+	mmcli -m $modem_no --messaging-list-sms
 elif [ $selector == 'read' ]
 then
 	echo "Showing message with selected id."
 	id=$(gum input --placeholder "message id")
-	mmcli -m 0 -s $id
+	mmcli -m $modem_no -s $id
 elif [ $selector == 'create' ]
 then
 	echo "Specify receipient number:"
 	message=$(gum input --placeholder "receipient number")
 	echo "Write your message:"
 	number=$(gum write --placeholder "your message (<ESC> to end message)")
-	mmcli -m 0 --messaging-create-sms="text='$message' ,number='$number'"
+	mmcli -m $modem_no --messaging-create-sms="text='$message' ,number='$number'"
 elif [ $selector == 'send' ]
 then
 	echo "Sending message with selected id."
@@ -26,5 +27,5 @@ elif [ $selector == 'delete' ]
 then
 	echo "Deleting message with selected id."
 	id=$(gum input --placeholder "message id")
-	mmcli -m 0 --messaging-delete-sms=$id
+	mmcli -m $modem_no --messaging-delete-sms=$id
 fi
