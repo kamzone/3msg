111b4ec918e1e74589f5c2efd87745869388cd6e
kamzone <macidlowskikacper20@lo3.gdynia.pl>
2023-01-09 10:05:12 +0000

    Added support for modem_no variable in simple-3msg

--- simple-3msg.sh
+++ simple-3msg.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 # Created by kam021m
+modem_no=0 # Set modem nuber value, determined using mmcli -L (0 is usually correct)
+
 if [ $1 == 'list' ]
 then
 	echo "Listing all messages id's and states."
-	mmcli -m 0 --messaging-list-sms
+	mmcli -m $modem_no --messaging-list-sms
 elif [ $1 == 'read' ]
 then
 	echo "Showing message with selected id."
-	mmcli -m 0 -s /org/freedesktop/ModemManager1/SMS/${2}
+	mmcli -m $modem_no -s /org/freedesktop/ModemManager1/SMS/${2}
 elif [ $1 == 'create' ]
 then
 	echo "Specify recipient number:"
 	read number
 	echo "Write your message:"
 	read message
-	mmcli -m 0 --messaging-create-sms="text='$message' ,number='$number'"
+	mmcli -m $modem_no --messaging-create-sms="text='$message' ,number='$number'"
 elif [ $1 == 'send' ]
 then
 	echo "Sending message with selected id."
@@ -22,5 +24,5 @@ then
 elif [ $1 == 'delete' ]
 then
 	echo "Deleting message with selected id."
-	mmcli -m 0 --messaging-delete-sms=$2
+	mmcli -m $modem_no --messaging-delete-sms=$2
 fi
